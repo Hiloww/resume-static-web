f033d1b52a8adce2b8499e733f4a0cf764fcc334
Hilda Abena Wilson <hildaabena.wilson@gmail.com>
2024-08-09 21:52:09 +0000

    Initial project commit

---
+++ data/profile.js
@@ -0,0 +1,183 @@
+const profileData = {
+  title: "Curriculum Vitae",
+  name: "Hilda Abena Wilson",
+  sub_title: "Information Technology Student | Cybersecurity Enthusiast",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `I am an Information Technology student with a strong passion for cybersecurity, particularly in the field of Digital Forensics, as well as Computer Networking and Security. Throughout my studies, I have developed basic knowledge and skills in Digital Forensics, and an in-depth understanding and practice in Computer Networking. I am always eager to learn and take on new challenges, and I am committed to using my skills to help organizations stay safe from cyber threats and maintain secure computer networks.`,
+    contact: {
+      email: "hildaabena.wilson@gmail.com",
+      phone: "0557009665",
+      address: "Greater Accra, Ghana",
+    },
+  },
+  links: [
+    {
+      title: "LinkedIn",
+      src: "https://www.linkedin.com/in/hilda-abena-wilson",
+      iconClass: "fa-brands fa-linkedin-in",
+    },
+  ],
+  skills: [
+    {
+      title: "Network Infrastructure Management",
+      value: "VLAN Configuration, Network Bill of Quantities, Dynamic and Static Routing",
+    },
+    {
+      title: "Operating System",
+      value: "UNIX - Linux",
+    },
+    {
+      title: "Programming Languages",
+      value: "Shell Scripting, Python, Java",
+    },
+    {
+      title: "Network Troubleshooting and Optimization",
+      value: "DHCPv4/DHCPv6, Spanning Tree Protocol (STP), Network Address Translation (NAT), Access Control Lists (ACLs)",
+    },
+    {
+      title: "Analytical Skills",
+      value: "Critical thinking and problem-solving",
+    },
+    {
+      title: "Time Management",
+      value: "Efficient task prioritization and execution",
+    },
+    {
+      title: "Digital Forensics",
+      value: "Basic File Systems Analysis - Volatility",
+    },
+  ],
+  experiences: [
+    {
+      organization: "Agricultural Development Bank",
+      title: "Industrial Attachment",
+      date: "Oct 2023 - Nov 2023",
+      details: [
+        "Gained experience in banking procedures and protocols.",
+        "Worked on HP Printers and remote networking.",
+        "Enhanced adaptability to different work environments and situations.",
+      ],
+    },
+    {
+      organization: "University of Cape Coast, Ghana",
+      title: "digiCAP Computer Networking and Security Internship",
+      date: "Ongoing since Oct 2022",
+      details: [
+        "VLAN Configuration and Design.",
+        "Network Bill of Quantities.",
+        "Dynamic and Static Routing Configuration.",
+        "DHCPv4 and DHCPv6 Implementation.",
+        "Spanning Tree Protocol (STP) Setup.",
+        "Network Address Translation (NAT) and Access Control Lists (ACLs).",
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "White Space Consult - Toolbox",
+      duration: "Group Project",
+      desc: `Took a significant part in the collaborative development of a project aimed at enlisting high-caliber artisans, including plumbers, electricians, laundry professionals, and others, to offer top-quality services tailored to the needs of students.`,
+    },
+  ],
+  education: [
+    {
+      alma: "University of Cape Coast",
+      duration: "2020 - Present",
+      std: "Bachelor of Science in Information Technology",
+      details: [
+        "Course Representative (Information Literacy Skills) - 01/2020 – present",
+        "Executive Secretary of Ladies in Tech Ghana, UCC Chapter",
+        "Student, Member, Mentee of Women in Cyber Security Africa Affiliate (WiCyS)",
+        "Programme Representative - digiCAP Computer Networking and Security",
+        "2x Dean's Award Recipient - 2020/2021 and 2021/2022 Academic Year",
+        "Competitor (Team Coastal Antidote) - 2024 Washington DC, Cyber 9/12 Strategy Challenge",
+        "Competitor (Team Coastal Antidote) - 2024 Geneva, Cyber 9/12 Strategy Challenge",
+      ],
+    },
+    {
+      alma: "Methodist Girls' Senior High School",
+      duration: "2017 - 2020",
+      std: "Senior High School (WASSCE)",
+      location: "Koforidua, Mamfe, Ghana",
+    },
+    {
+      alma: "Alphanare International Academy",
+      duration: "2012 - 2017",
+      std: "Basic Education",
+      location: "Accra, Ghana",
+    },
+  ],
+  certifications: [
+    {
+      desc: "(ISC)2 Certified in Cybersecurity ((ISC)2 CC) - Credential ID 1963631",
+    },
+    {
+      desc: "3x School of Physical Sciences Dean's Award Recipient - 2020/2021, 2021/2022, 2023/2024",
+    },
+    {
+      desc: "Introduction to Cyber Security",
+    },
+    {
+      desc: "AWS Certified Cloud Practitioner (Amazon Web Services (AWS), AWS04039005)",
+    },
+    {
+      desc: "Network Defense from Cisco Skills for All Academy",
+    },
+    {
+      desc: "Python Essentials 1 from CISCO",
+    },
+    {
+      desc: "TELECEL Female Engineering Student Sponsorship Awardee",
+    },
+    {
+      desc: "Digital Forensics Essentials (DFE) from EC-Council",
+    },
+    {
+      desc: "Pre Security Learning Path",
+    },
+  ],
+  training: [
+    {
+      desc: "digiCAP Computer Networking and Security, University of Cape Coast, Ghana - Oct 2022 – Present",
+    },
+    {
+      desc: "Skill Up League 2.0 Mentorship Program, WiCyS - Sept 2023 – Dec 2023",
+    },
+    {
+      desc: "WiCyS Security Training Scholarship",
+    },
+    {
+      desc: "Enterprise Security - Bertelsmann at Udacity - Dec 2023 – Apr 2024",
+    },
+  ],
+  references: [
+    {
+      name: "Mr. Emmanuel Vincent Mensah",
+      title: "Assistant Systems Analyst",
+      organization: "University of Cape Coast",
+      email: "evmensah@ucc.edu.gh",
+      phone: "0249124808",
+    },
+    {
+      name: "Dr. Emmanuel Dortey Tetteh",
+      title: "Lecturer and Senior Member",
+      organization: "University of Cape Coast",
+      email: "etetteh@ucc.edu.gh",
+      phone: "0244572711",
+    },
+    {
+      name: "Mr. Paul Kobina Arhin Jnr.",
+      title: "Lecturer",
+      organization: "University of Cape Coast",
+      email: "paul.arhin@ucc.edu.gh",
+      phone: "0246830771",
+    },
+    {
+      name: "Dela Newton",
+      title: "Branch Manager",
+      organization: "Agricultural Development Bank",
+      phone: "0208163064",
+    },
+  ],
+};
